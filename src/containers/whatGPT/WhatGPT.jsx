ecca3a30dc762e9a0f74e4866871595c77f72d6d
Roberto Tavitas <robertomirandatavitas@gmail.com>
2021-12-01 11:39:53 -0600

    what section done, still have to change text and info latter on

--- src/containers/whatGPT/WhatGPT.jsx
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
+
 import './whatGPT.css';
 
 const WhatGPT = () => {
     return (
-        <div>
-          WhatGPT  
+        <div id="what" className="what section__margin">
+
+          <div className="what-feature">
+            <Feature 
+              title="What is lorem ipsum?" 
+              text="Lorem ipsum dolor sit amet."
+            />
+          </div>
+
+          <div className="what-heading">
+            <h1 className="gradient__text">Lorem ipsum dolor sit amet.</h1>
+            <p>Lorem ipsum dolor.</p>
+          </div>
+
+          <div className="what-container">
+            <Feature 
+                title="What is lorem ipsum?" 
+                text="Lorem ipsum dolor sit amet."
+              />
+              <Feature 
+              title="What is lorem ipsum?" 
+              text="Lorem ipsum dolor sit amet."
+            />
+            <Feature 
+              title="What is lorem ipsum?" 
+              text="Lorem ipsum dolor sit amet."
+            />
+          </div>
         </div>
     )
 }
